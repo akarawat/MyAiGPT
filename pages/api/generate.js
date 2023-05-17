5201edb62c221b13008b76b1b323eef2e5182c4e
Akarawat Panwilai <akarawat.bis@gmail.com>
2023-05-17 10:14:36 +0700

    change model

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -27,7 +27,8 @@ export default async function (req, res) {
 
   try {
     const completion = await openai.createCompletion({
-      model: "text-davinci-003",
+      // model: "text-davinci-003",
+      model: "gpt-3.5-turbo",
       prompt: generatePrompt(animal),
       temperature: 0.6,
     });
